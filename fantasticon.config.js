7e62ae6a5111ebbf93c639bb1c461510f272f798
Skyler Berg <skylerberg@users.noreply.github.com>
2023-07-24 23:39:53 -0700

    Initial commit

---
+++ fantasticon.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  inputDir: './icons', // (required)
+  outputDir: './src/assets/fonts', // (required)
+  fontTypes: ['eot', 'woff', 'woff2'],
+  assetTypes: ['css'],
+  fontHeight: 1000,
+  descent: 150,
+  formatOptions: {
+    svg: {
+      centerHorizontally: true,
+      centerVertically: true,
+    },
+  },
+};
