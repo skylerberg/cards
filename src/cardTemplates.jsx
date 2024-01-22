f67f3a20407b700b8e192c8609fd1dd1670be112
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2024-01-22 13:46:24 -0800

    Start adding VP symbol to cards

--- src/cardTemplates.jsx
+++ src/cardTemplates.jsx
@@ -1,4 +1,32 @@
+import victoryPointsImage from './assets/general/victory-points.svg';
+
 const allyImages = import.meta.glob('./assets/images/allies/*.png', { eager: true });
+const rocketImages = import.meta.glob('./assets/images/rockets/*.png', { eager: true });
+
+export function FactionCard({name, score, ability}) {
+  return (
+    <div className="card" style={
+      {
+        backgroundSize: '2.5in',
+        backgroundRepeat: 'no-repeat',
+        backgroundPositionY: 'center',
+      }
+    }>
+      <div className="ally-top-line">
+        <div className="ally-name-box overlay-box">
+          {name}
+          <div className="card-type">Faction Card</div>
+        </div>
+        <div className="ally-score-box overlay-box">
+          {score}
+        </div>
+      </div>
+      <div className="ally-action-box overlay-box">
+        {ability}
+      </div>
+    </div>
+  );
+}
 
 export function AllyCard({name, score, ability}) {
   const pngName = `${name.toLowerCase().replaceAll(' ', '-')}.png`;
@@ -18,7 +46,19 @@ export function AllyCard({name, score, ability}) {
           <div className="card-type">Ally</div>
         </div>
         <div className="ally-score-box overlay-box">
-          {score}
+          <img src={victoryPointsImage} style={
+            {
+              marginBottom: '-4px',
+            }
+          }/>
+          <span style={
+            {
+              position: 'absolute',
+              margin: 'auto',
+            }
+          }>
+            {score}
+          </span>
         </div>
       </div>
       <div className="ally-action-box overlay-box">
@@ -29,8 +69,16 @@ export function AllyCard({name, score, ability}) {
 }
 
 export function RocketCard({name, score, ability}) {
+  const imageUrl = rocketImages[`./assets/images/rockets/rocket.png`].default;
   return (
-    <div className="card">
+    <div className="card" style={
+      {
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: '2.5in',
+        backgroundRepeat: 'no-repeat',
+        backgroundPositionY: 'center',
+      }
+    }>
       <div className="ally-top-line">
         <div className="ally-name-box overlay-box">
           {name}
